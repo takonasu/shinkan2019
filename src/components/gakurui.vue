0295764c6673d605356323cf8846f1ee87064b2e
suzuki kensuke <s.kensuke0917@gmail.com>
2019-03-13 16:30:33 +0900

    add:日時・参加費の情報を追加

--- src/components/gakurui.vue
+++ src/components/gakurui.vue
@@ -2,53 +2,91 @@
   <div>
     <v-flex>
       <v-container>
-
         <v-card>
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb-0">日時</h3>
+              <h3 class="headline mb-0">学類懇親会</h3>
             </div>
           </v-card-title>
           <v-card-text>
-            <div> 草 </div>
+            <div>学類の新歓行事を締めくくる会です。
+              <!-- 内容が薄いからどうにかしよう -->
+              ぜひ奮ってご参加を！
+            </div>
           </v-card-text>
 
+          <v-container>
+            <v-card>
+              <v-card-title primary-title>
+                <div>
+                  <h3 class="headline mb-0">日時</h3>
+                </div>
+              </v-card-title>
+              <v-card-text>
+                <h3>4月13日(土) 17時30分開始</h3>
+              </v-card-text>
+            </v-card>
+          </v-container>
+
+          <v-container>
+            <v-card>
+              <v-card-title primary-title>
+                <div>
+                  <h3 class="headline mb-0">参加費</h3>
+                </div>
+              </v-card-title>
+              <v-card-text>
+                <div>
+                  <h3>不要</h3>
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-container>
+
+          <v-container>
+            <v-card>
+              <v-card-title primary-title>
+                <div>
+                  <h3 class="headline mb-0">集合場所</h3>
+                </div>
+              </v-card-title>
+              <v-card-text>
+                <div>各自集まりやすい場所に来てください。各集合場所には法被を着た新歓委員が待機しています。</div>
+                <div>集合時間はLINE@にてお知らせいたします。</div>
+              </v-card-text>
+
+              <v-card-text>
+                <h3>集合場所④</h3>
+                <div>つくば駅改札前</div>
+                <div>〒305-0031 茨城県つくば市吾妻２丁目６−１２８</div>
+              </v-card-text>
+              <div class="ggmap">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2227.953477858671!2d140.11041961759028!3d36.08255944198964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3487ddbc353229bd!2z44Gk44GP44Gw6aeF!5e0!3m2!1sja!2sjp!4v1552455413535"
+                  width="600"
+                  height="450"
+                  frameborder="0"
+                  style="border:0"
+                  allowfullscreen
+                ></iframe>
+              </div>
+            </v-card>
+          </v-container>
         </v-card>
       </v-container>
-
-      <v-container>
-        <v-card>
-          <v-card-text>
-            <h3>こんにちは</h3>
-            <p>今日はいいお天気ですね</p>
-          </v-card-text>
-        </v-card>
-      </v-container>
-
-      <v-container>
-        <v-card>
-          <v-card-text>
-            <h3>こんにちは</h3>
-            <p>今日はいいお天気ですね</p>
-          </v-card-text>
-        </v-card>
-      </v-container>
-
-
     </v-flex>
-
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.'
-      }
-    }
+export default {
+  data() {
+    return {
+      card_text:
+        "Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat."
+    };
   }
-
+};
 </script>
 
 <style>
