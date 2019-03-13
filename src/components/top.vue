917a18f85c4e3c0e4f1e17eff4928bd7c4981449
tako <tako@TakoBookPro.pc.sohosai.tsukuba.ac.jp>
2019-03-13 14:43:37 +0900

    ページ追加・トップページの修正

--- src/components/top.vue
+++ src/components/top.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-layout row wrap>
-      <v-flex xs6 v-for="n in card_lists" v-bind:key="n.id">
+      <v-flex xs12 md6 v-for="n in card_lists" v-bind:key="n.id">
         <v-container>
           <v-card>
             <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="2.75"></v-img>
@@ -13,12 +13,14 @@
               </div>
             </v-card-title>
 
-            <v-card-actions><router-link v-bind:to="n.link" style="text-decoration: none;">
-              <v-btn color="blue lighten-1" dark>
-                  移動 
-              </v-btn></router-link>
+            <v-card-actions>
+              <router-link v-bind:to="n.link" style="text-decoration: none;">
+                <v-btn color="blue lighten-1" dark>
+                  移動
+                </v-btn>
+              </router-link>
             </v-card-actions>
-                            
+
 
           </v-card>
         </v-container>
@@ -36,7 +38,7 @@
             id: 1,
             title: "新歓合宿",
             text: "合宿に行きます",
-            link: "gashuku"
+            link: "camp"
           },
           {
             id: 2,
@@ -52,6 +54,42 @@
           },
           {
             id: 4,
+            title: "学類懇親会",
+            text: "情報科学類で懇親会するよ！",
+            link: "gakurui"
+          },
+          {
+            id: 5,
+            title: "学群懇親会",
+            text: "情報学群で懇親会するよ！",
+            link: "gakugun"
+          },
+          {
+            id: 6,
+            title: "入学式",
+            text: "パンフの日にち間違えてるから気をつけて！",
+            link: "enter"
+          },
+          {
+            id: 7,
+            title: "プレイスメントテスト",
+            text: "は？なにこれ？",
+            link: "Eng_test"
+          },
+          {
+            id: 8,
+            title: "宿舎入居",
+            text: "宿舎に入る人はみてね！",
+            link: "shukusya"
+          },
+                    {
+            id: 9,
+            title: "理趣相談会",
+            text: "履修って何？先輩と一緒に登録しよう！",
+            link: "risyu"
+          },
+          {
+            id: 10,
             title: "その他",
             text: "スライム",
             link: "other"
