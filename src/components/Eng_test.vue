1f80def93095b18c159358d39df432077e4660d5
suzuki kensuke <s.kensuke0917@gmail.com>
2019-03-13 17:56:04 +0900

    fix:toeflように変えた

--- src/components/Eng_test.vue
+++ src/components/Eng_test.vue
@@ -5,14 +5,11 @@
         <v-card>
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb-0">学群懇親会</h3>
+              <h3 class="headline mb-0">プレイスメントテストってなんや！！</h3>
             </div>
           </v-card-title>
           <v-card-text>
-            <div>
-              情報メディア創成学類や知識情報図書館学類の新入生と全体で関われる最初のチャンスです。
-              同じ学群内での共通の趣味を持った人を探してみるのも良いでしょう！
-            </div>
+            <div>説明するよ！</div>
           </v-card-text>
 
           <v-container>
@@ -23,7 +20,7 @@
                 </div>
               </v-card-title>
               <v-card-text>
-                <h3>4月20日(土) 17時00分開始</h3>
+                <h3>日時だよ</h3>
               </v-card-text>
             </v-card>
           </v-container>
@@ -37,7 +34,7 @@
               </v-card-title>
               <v-card-text>
                 <div>
-                  <h3>不要</h3>
+                  <h3>ここいらないかも</h3>
                 </div>
               </v-card-text>
             </v-card>
@@ -52,55 +49,8 @@
               </v-card-title>
 
               <v-card-text>
-                <div>筑波大学 グローバルビレッジ</div>
-                <div>〒305-0005 茨城県つくば市天久保3丁目15-1</div>
+                <div>教室名</div>
               </v-card-text>
-
-              <div class="ggmap">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d471.6816134413474!2d140.10422358397958!3d36.09620398148769!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60220c74ba64fe57%3A0x3b603182ac34fbaa!2z562R5rOi5aSn5a2mIOOCsOODreODvOODkOODq-ODtOOCo-ODrOODg-OCuCDjgrPjg5_jg6Xjg4vjg4bjgqPjgrnjg4bjg7zjgrfjg6fjg7M!5e0!3m2!1sja!2sjp!4v1552462754340"
-                  width="600"
-                  height="450"
-                  frameborder="0"
-                  style="border:0"
-                  allowfullscreen
-                ></iframe>
-              </div>
-
-              <v-card-text>こちらは開催場所になります。
-                <br>集合場所は以下を参照してください。
-                <!-- 集合場所より近い場合は直接来てもらって構いません。 -->
-              </v-card-text>
-            </v-card>
-          </v-container>
-
-          <v-container>
-            <v-card>
-              <v-card-title primary-title>
-                <div>
-                  <h3 class="headline mb-0">集合場所</h3>
-                </div>
-              </v-card-title>
-              <v-card-text>
-                <div>集合場所には法被を着た新歓委員が待機しています。</div>
-                <div class="font-weight-bold">集合時間は16時30分です。</div>
-                <div>※直接グローバルビレッジに来ていただいても構いません。</div>
-              </v-card-text>
-
-              <v-card-text>
-                <div>平砂学生宿舎 共用棟前</div>
-                <div>〒305-0005 茨城県つくば市天久保２丁目１</div>
-              </v-card-text>
-              <div class="ggmap">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d680.3404568447171!2d140.10330268839482!3d36.09777029955882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60220c7517f83bc3%3A0x61e61613464b8588!2z562R5rOi5aSn5a2mIOWtpueUn-Wuv-iIjuODu-emj-WIqeWOmueUn-aWveioreetieW5s-eggueUn-a0u-OCu-ODs-OCv-ODvOeuoeeQhuS6i-WLmeWupA!5e0!3m2!1sja!2sjp!4v1552462342110"
-                  width="600"
-                  height="450"
-                  frameborder="0"
-                  style="border:0"
-                  allowfullscreen
-                ></iframe>
-              </div>
             </v-card>
           </v-container>
         </v-card>
