272983722f1edd317e2b819b4b43e82009865a1f
suzuki kensuke <s.kensuke0917@gmail.com>
2019-03-13 16:41:29 +0900

    add:開催場所・集合場所を追加

--- src/components/gakurui.vue
+++ src/components/gakurui.vue
@@ -47,22 +47,52 @@
             <v-card>
               <v-card-title primary-title>
                 <div>
+                  <h3 class="headline mb-0">開催場所</h3>
+                </div>
+              </v-card-title>
+
+              <v-card-text>
+                <div>筑波大学 グローバルビレッジ</div>
+                <div>〒305-0005 茨城県つくば市天久保3丁目15-1</div>
+              </v-card-text>
+
+              <div class="ggmap">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d471.6816134413474!2d140.10422358397958!3d36.09620398148769!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60220c74ba64fe57%3A0x3b603182ac34fbaa!2z562R5rOi5aSn5a2mIOOCsOODreODvOODkOODq-ODtOOCo-ODrOODg-OCuCDjgrPjg5_jg6Xjg4vjg4bjgqPjgrnjg4bjg7zjgrfjg6fjg7M!5e0!3m2!1sja!2sjp!4v1552462754340"
+                  width="600"
+                  height="450"
+                  frameborder="0"
+                  style="border:0"
+                  allowfullscreen
+                ></iframe>
+              </div>
+
+              <v-card-text>こちらは開催場所になります。
+                <br>集合場所は以下を参照してください。
+                <!-- 集合場所より近い場合は直接来てもらって構いません。 -->
+              </v-card-text>
+            </v-card>
+          </v-container>
+
+          <v-container>
+            <v-card>
+              <v-card-title primary-title>
+                <div>
                   <h3 class="headline mb-0">集合場所</h3>
                 </div>
               </v-card-title>
               <v-card-text>
-                <div>各自集まりやすい場所に来てください。各集合場所には法被を着た新歓委員が待機しています。</div>
-                <div>集合時間はLINE@にてお知らせいたします。</div>
+                <div>集合場所には法被を着た新歓委員が待機しています。</div>
+                <div>集合時間は17時です。</div>
               </v-card-text>
 
               <v-card-text>
-                <h3>集合場所④</h3>
-                <div>つくば駅改札前</div>
-                <div>〒305-0031 茨城県つくば市吾妻２丁目６−１２８</div>
+                <div>平砂学生宿舎 共用棟前</div>
+                <div>〒305-0005 茨城県つくば市天久保２丁目１</div>
               </v-card-text>
               <div class="ggmap">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2227.953477858671!2d140.11041961759028!3d36.08255944198964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3487ddbc353229bd!2z44Gk44GP44Gw6aeF!5e0!3m2!1sja!2sjp!4v1552455413535"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d680.3404568447171!2d140.10330268839482!3d36.09777029955882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60220c7517f83bc3%3A0x61e61613464b8588!2z562R5rOi5aSn5a2mIOWtpueUn-Wuv-iIjuODu-emj-WIqeWOmueUn-aWveioreetieW5s-eggueUn-a0u-OCu-ODs-OCv-ODvOeuoeeQhuS6i-WLmeWupA!5e0!3m2!1sja!2sjp!4v1552462342110"
                   width="600"
                   height="450"
                   frameborder="0"
