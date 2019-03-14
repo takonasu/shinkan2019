d81afc7985eb9498f3d3feef0d370888925b7029
suzuki kensuke <s.kensuke0917@gmail.com>
2019-03-14 14:24:43 +0900

    add:場所のカードを追加

--- src/components/shukusya.vue
+++ src/components/shukusya.vue
@@ -10,7 +10,10 @@
           </v-card-title>
           <v-card-text>
             <h4>※これは学生宿舎に入居する人のための情報です</h4>
-            <div>学生宿舎に入居する新入生のお手伝いをします！</div>
+            <div>
+              学生宿舎に入居する新入生の荷物運びや家具の設置等の補助、必要物購入の支援などの
+              お手伝いをします。
+            </div>
           </v-card-text>
 
           <v-container>
@@ -22,6 +25,19 @@
               </v-card-title>
               <v-card-text>
                 <h3>4月4日(木) 9:00~15:00</h3>
+              </v-card-text>
+            </v-card>
+          </v-container>
+
+          <v-container>
+            <v-card>
+              <v-card-title primary-title>
+                <div>
+                  <h3 class="headline mb-0">場所</h3>
+                </div>
+              </v-card-title>
+              <v-card-text>
+                <h3>平砂学生宿舎付近、グローバルビレッジ付近</h3>
               </v-card-text>
             </v-card>
           </v-container>
