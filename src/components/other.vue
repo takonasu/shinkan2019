d0e6efec1a503f86ea15fc16c8676697a757f6a9
suzuki kensuke <s.kensuke0917@gmail.com>
2019-03-14 17:31:06 +0900

    add:twitterのcardを追加

--- src/components/other.vue
+++ src/components/other.vue
@@ -2,53 +2,59 @@
   <div>
     <v-flex>
       <v-container>
-
         <v-card>
+          <v-img
+            src="https://pbs.twimg.com/profile_images/1041278929854558210/VMcuApjK_400x400.jpg"
+            aspect-ratio="1.5"
+          ></v-img>
+
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb-0">日時</h3>
+              <h3 class="headline mb-0">oshun(@uk_osy)</h3>
             </div>
           </v-card-title>
-          <v-card-text>
-            <div> 草 </div>
-          </v-card-text>
-
         </v-card>
       </v-container>
 
       <v-container>
         <v-card>
-          <v-card-text>
-            <h3>こんにちは</h3>
-            <p>今日はいいお天気ですね</p>
-          </v-card-text>
+          <v-img
+            src="https://pbs.twimg.com/profile_images/1079755767119409153/aTlUrEPU_400x400.jpg"
+            aspect-ratio="1.5"
+          ></v-img>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">takonasu(@ITF_tako)</h3>
+            </div>
+          </v-card-title>
         </v-card>
       </v-container>
 
       <v-container>
         <v-card>
-          <v-card-text>
-            <h3>こんにちは</h3>
-            <p>今日はいいお天気ですね</p>
-          </v-card-text>
+          <v-img src="/static/szk.jpg" aspect-ratio="1.5"></v-img>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">szk18(@ITF_szk)</h3>
+            </div>
+          </v-card-title>
         </v-card>
       </v-container>
-
-
     </v-flex>
-
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.'
-      }
-    }
+export default {
+  data() {
+    return {
+      card_text:
+        "Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat."
+    };
   }
-
+};
 </script>
 
 <style>
