bae78abd3646ab876e3292a7be36281213c2dbb1
suzuki kensuke <s.kensuke0917@gmail.com>
2019-03-22 17:01:47 +0900

    fix:npn run lintfixを忘れてた

--- src/components/top.vue
+++ src/components/top.vue
@@ -33,68 +33,68 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       page_lists: [
         {
           id: 1,
-          title: "宿舎入居",
-          text: "宿舎に入る人はみてね！",
-          link: "lodging",
-          img: "hirakyo.jpg"
+          title: '宿舎入居',
+          text: '宿舎に入る人はみてね！',
+          link: 'lodging',
+          img: 'hirakyo.jpg'
         },
         {
           id: 2,
-          title: "入学前食事会",
-          text: "百香亭という中華飯店に行きます",
-          link: "meal",
-          img: "hyakkou.jpg"
+          title: '入学前食事会',
+          text: '百香亭という中華飯店に行きます',
+          link: 'meal',
+          img: 'hyakkou.jpg'
         },
         {
           id: 3,
-          title: "入学式",
-          text: "お待ちかねの入学式です！",
-          link: "entrance",
-          img: "kaikan.jpg"
+          title: '入学式',
+          text: 'お待ちかねの入学式です！',
+          link: 'entrance',
+          img: 'kaikan.jpg'
         },
         {
           id: 4,
-          title: "プレイスメントテスト",
-          text: "は？なにこれ？",
-          link: "test",
-          img: "2gaku.jpg"
+          title: 'プレイスメントテスト',
+          text: 'は？なにこれ？',
+          link: 'test',
+          img: '2gaku.jpg'
         },
         {
           id: 5,
-          title: "新歓合宿",
-          text: "合宿に行きます",
-          link: "camp",
-          img: "BQ.jpg"
+          title: '新歓合宿',
+          text: '合宿に行きます',
+          link: 'camp',
+          img: 'BQ.jpg'
         },
         {
           id: 6,
-          title: "履修相談会",
-          text: "履修って何？先輩と一緒に登録しよう！",
-          link: "registration",
-          img: "lounge.jpg"
+          title: '履修相談会',
+          text: '履修って何？先輩と一緒に登録しよう！',
+          link: 'registration',
+          img: 'lounge.jpg'
         },
         {
           id: 7,
-          title: "情報科学類懇親会",
-          text: "情報科学類で懇親会するよ！",
-          link: "department",
-          img: "coinskonshin.jpg"
+          title: '情報科学類懇親会',
+          text: '情報科学類で懇親会するよ！',
+          link: 'department',
+          img: 'coinskonshin.jpg'
         },
         {
           id: 8,
-          title: "情報学群懇親会",
-          text: "情報学群で懇親会するよ！",
-          link: "faculty",
-          img: "gurobi.jpg"
+          title: '情報学群懇親会',
+          text: '情報学群で懇親会するよ！',
+          link: 'faculty',
+          img: 'gurobi.jpg'
         }
       ],
       alert: true
-    };
+    }
   }
-};
+}
 </script>
