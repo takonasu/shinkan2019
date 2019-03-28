cb52afbbf5eadd1b9d22882bccb0bba55b9f28c8
ooshun <yos.kch.gkg.tnnt@gmail.com>
2019-03-28 11:55:04 +0900

    事業計画書と予算書用のページを追加

---
+++ src/components/document.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-container>
+      <v-card>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">平成31年度筑波大学情報科学類新入生歓迎委員会事業計画書 予算書</h3>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          <h4></h4>
+          <div></div>
+        </v-card-text>
+
+        <v-container>
+          <v-card class="mb-4">
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">事業計画書</h3>
+              </div>
+            </v-card-title>
+            <v-card-text>
+              <h3>事業計画書</h3>
+            </v-card-text>
+          </v-card>
+          <v-card class="mb-4">
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">予算書</h3>
+              </div>
+            </v-card-title>
+            <v-card-text>
+              <h3>予算書</h3>
+            </v-card-text>
+          </v-card>
+        </v-container>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      card_text:
+        "Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat."
+    };
+  }
+};
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -10,63 +10,71 @@ import gakugun from '@/components/gakugun'
 import gakurui from '@/components/gakurui'
 import risyu from '@/components/risyu'
 import shukusya from '@/components/shukusya'
+import document from '@/components/document'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'top',
-      component: top
-    },
-    {
-      path: '/camp',
-      name: 'gashuku',
-      component: gashuku
-    },
-    {
-      path: '/meal',
-      name: 'gohan',
-      component: gohan
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: test
-    },
-    {
-      path: '/entrance',
-      name: 'enter',
-      component: enter
-    },
-    {
-      path: '/faculty',
-      name: 'gakugun',
-      component: gakugun
-    },
-    {
-      path: '/lodging',
-      name: 'shukusya',
-      component: shukusya
-    },
-    {
-      path: '/department',
-      name: 'gakurui',
-      component: gakurui
-    },
-    {
-      path: '/registration',
-      name: 'risyu',
-      component: risyu
-    },
-    {
-      path: '/author',
-      name: 'other',
-      component: other
-    }
+  routes: [{
+    path: '/',
+    name: 'top',
+    component: top
+  },
+  {
+    path: '/camp',
+    name: 'gashuku',
+    component: gashuku
+  },
+  {
+    path: '/meal',
+    name: 'gohan',
+    component: gohan
+  },
+  {
+    path: '/test',
+    name: 'test',
+    component: test
+  },
+  {
+    path: '/entrance',
+    name: 'enter',
+    component: enter
+  },
+  {
+    path: '/faculty',
+    name: 'gakugun',
+    component: gakugun
+  },
+  {
+    path: '/lodging',
+    name: 'shukusya',
+    component: shukusya
+  },
+  {
+    path: '/department',
+    name: 'gakurui',
+    component: gakurui
+  },
+  {
+    path: '/registration',
+    name: 'risyu',
+    component: risyu
+  },
+  {
+    path: '/author',
+    name: 'other',
+    component: other
+  },
+  {
+    path: '/document',
+    name: 'document',
+    component: document
+  }
   ],
   scrollBehavior (to, from, savedPosition) {
-    return { x: 0, y: 0 }
+    return {
+      x: 0,
+      y: 0
+    }
   }
 })
