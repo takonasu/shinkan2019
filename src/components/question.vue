9595012057e6525a2d5a8311d785cf95fda70c41
suzuki kensuke <s.kensuke0917@gmail.com>
2019-03-30 21:55:59 +0900

    delete:cardを削除

--- src/components/question.vue
+++ src/components/question.vue
@@ -1,68 +1,46 @@
 <template>
   <div>
-      <v-container>
+    <v-container>
+      <v-card class="mb-4">
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Q＆Aだよ！！</h3>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          <div>
+            皆さんから寄せられた質問の中で特に多かったものについて答えていきます！
+            わからないことがあったらLINE@でどんどん質問しましょう！
+          </div>
+        </v-card-text>
+
+        <v-container>
           <v-card class="mb-4">
-          <v-card-title primary-title>
-            <div>
-              <h3 class="headline mb-0">Q＆Aだよ！！</h3>
-            </div>
-          </v-card-title>
-          <v-card-text>
-            <div>
-              皆さんから寄せられた質問の中で特に多かったものについて答えていきます！
-              わからないことがあったらLINE@でどんどん質問しましょう！
-            </div>
-          </v-card-text>
-
-          <v-container>
-            <v-card class="mb-4">
-              <v-card-title primary-title>
-                <div>
-                  <h3 class="headline mb-0">日時</h3>
-                </div>
-              </v-card-title>
-              <v-card-text>
-                <h3>4月13日(土) 10:00~16:00</h3>
-                <h3>4月14日(日) 10:00~16:00</h3>
-              </v-card-text>
-            </v-card>
-            <v-card class="mb-4">
-              <v-card-title primary-title>
-                <div>
-                  <h3 class="headline mb-0">持ち物</h3>
-                </div>
-              </v-card-title>
-              <v-card-text>
-                <h3>履修要覧、開設科目一覧、学生証、統一認証のパスワードが書いてある紙</h3>
-              </v-card-text>
-            </v-card>
-            <v-card class="mb-4">
-              <v-card-title primary-title>
-                <div>
-                  <h3 class="headline mb-0">会場</h3>
-                </div>
-              </v-card-title>
-
-              <v-card-text>
-                <h3>全学計算機室</h3>
-                <div>詳しい場所は確定し次第掲載します。</div>
-              </v-card-text>
-            </v-card>
-          </v-container>
-        </v-card>
-      </v-container>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">日時</h3>
+              </div>
+            </v-card-title>
+            <v-card-text>
+              <h3>4月13日(土) 10:00~16:00</h3>
+              <h3>4月14日(日) 10:00~16:00</h3>
+            </v-card-text>
+          </v-card>
+        </v-container>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.'
-    }
+      card_text:
+        "Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat."
+    };
   }
-}
-
+};
 </script>
 
 <style>
