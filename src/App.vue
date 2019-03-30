fda8676a180ee4aff5cd312aecef3d4e9f368905
suzuki kensuke <s.kensuke0917@gmail.com>
2019-03-30 23:15:40 +0900

    add:ツールバーに追加

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,12 @@
       </v-toolbar>
       <v-navigation-drawer v-model="drawer" fixed app>
         <v-list dense>
-          <v-list-tile v-for="item in page_lists" :key="item.title" @click="$router.push(item.link);drawer=!drawer"
-            class="mt-2">
+          <v-list-tile
+            v-for="item in page_lists"
+            :key="item.title"
+            @click="$router.push(item.link);drawer=!drawer"
+            class="mt-2"
+          >
             <v-list-tile-action>
               <v-icon>{{ item.icon }}</v-icon>
             </v-list-tile-action>
@@ -19,7 +23,6 @@
             </v-list-tile-content>
           </v-list-tile>
         </v-list>
-
       </v-navigation-drawer>
 
       <v-img src="/static/ishi.jpg" height="30vh" class="mt-5">
@@ -28,10 +31,9 @@
         </v-layout>
       </v-img>
       <div class="mb-4">
-
-<transition name="fade">
-        <router-view />
-</transition>
+        <transition name="fade">
+          <router-view/>
+        </transition>
       </div>
       <v-footer dark height="auto">
         <v-card class="flex" flat tile>
@@ -50,92 +52,101 @@
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data: () => ({
-    page_lists: [{
-      id: 0,
-      title: 'トップページ',
-      link: '/',
-      icon: 'dashboard'
-    }, {
-      id: 1,
-      title: '宿舎入居',
-      text: '宿舎に入る人はみてね！',
-      link: 'lodging',
-      icon: 'fas fa-home'
-    },
-    {
-      id: 2,
-      title: '入学前食事会',
-      text: '百香亭という中華飯店に行きます',
-      link: 'meal',
-      icon: 'fas fa-utensils'
-    },
-    {
-      id: 3,
-      title: '入学式',
-      text: 'お待ちかねの入学式です！',
-      link: 'entrance',
-      icon: 'fas fa-user-graduate'
-    },
-    {
-      id: 4,
-      title: 'プレイスメントテスト',
-      text: 'は？なにこれ？',
-      link: 'test',
-      icon: 'fas fa-pencil-alt'
-    },
-    {
-      id: 5,
-      title: '新歓合宿',
-      text: '合宿に行きます',
-      link: 'camp',
-      icon: 'fas fa-bed'
-    },
-    {
-      id: 6,
-      title: '履修相談会',
-      text: '履修って何？先輩と一緒に登録しよう！',
-      link: 'registration',
-      icon: 'fas fa-desktop'
-    },
-    {
-      id: 7,
-      title: '情報科学類懇親会',
-      text: '情報科学類で懇親会するよ！',
-      link: 'department',
-      icon: 'fas fa-handshake'
-    },
-    {
-      id: 8,
-      title: '情報学群懇親会',
-      text: '情報学群で懇親会するよ！',
-      link: 'faculty',
-      icon: 'fas fa-user-friends'
-    },
-    {
-      id: 9,
-      title: 'このサイトについて',
-      link: 'author',
-      icon: 'fas fa-feather'
-    }
+    page_lists: [
+      {
+        id: 0,
+        title: "トップページ",
+        link: "/",
+        icon: "dashboard"
+      },
+      {
+        id: 1,
+        title: "宿舎入居",
+        text: "宿舎に入る人はみてね！",
+        link: "lodging",
+        icon: "fas fa-home"
+      },
+      {
+        id: 2,
+        title: "入学前食事会",
+        text: "百香亭という中華飯店に行きます",
+        link: "meal",
+        icon: "fas fa-utensils"
+      },
+      {
+        id: 3,
+        title: "入学式",
+        text: "お待ちかねの入学式です！",
+        link: "entrance",
+        icon: "fas fa-user-graduate"
+      },
+      {
+        id: 4,
+        title: "プレイスメントテスト",
+        text: "は？なにこれ？",
+        link: "test",
+        icon: "fas fa-pencil-alt"
+      },
+      {
+        id: 5,
+        title: "新歓合宿",
+        text: "合宿に行きます",
+        link: "camp",
+        icon: "fas fa-bed"
+      },
+      {
+        id: 6,
+        title: "履修相談会",
+        text: "履修って何？先輩と一緒に登録しよう！",
+        link: "registration",
+        icon: "fas fa-desktop"
+      },
+      {
+        id: 7,
+        title: "情報科学類懇親会",
+        text: "情報科学類で懇親会するよ！",
+        link: "department",
+        icon: "fas fa-handshake"
+      },
+      {
+        id: 8,
+        title: "情報学群懇親会",
+        text: "情報学群で懇親会するよ！",
+        link: "faculty",
+        icon: "fas fa-user-friends"
+      },
+      {
+        id: 9,
+        title: "Q＆A",
+        text: "よく聞かれる質問と回答をまとめたよ！",
+        link: "question",
+        icon: "fas fa-question"
+      },
+      {
+        id: 10,
+        title: "このサイトについて",
+        link: "author",
+        icon: "fas fa-feather"
+      }
     ],
     drawer: false,
     drawerRight: null,
     right: false,
     left: false
   })
-
-}
-
+};
 </script>
 
 <style scoped>
-.fade-enter-active, .fade-leave-active {
+.fade-enter-active,
+.fade-leave-active {
   will-change: opacity;
   transition: opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
 }
-.fade-enter, .fade-leave-to {
-  opacity: 0
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
